e7a8665089f653ed1896fd594db41121ae8cca63
Kbowen2001 <Kiarrabowen2001@gmail.com>
2026-02-17 14:14:10 -0700

    ad

--- routes/index.js
+++ routes/index.js
@@ -4,3 +4,7 @@ const routes = require("express").Router();
 routes.get("/", myController.awesomeFunction);
 
 module.exports = routes;
+
+app.listen(PORT, () => {
+  console.log(`Test server running on port: ${PORT}`);
+});
